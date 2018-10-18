b91e740351642996e660ff29dcf7e813785ca25c
ChrisIrvine <christopher@irvinegroup.eu>
2018-10-18 09:48:40 +0100

    1 more test passed, redeveloped handling in HorizontalTransporter

--- src/HorizontalTransporter.java
+++ src/HorizontalTransporter.java
@@ -1,6 +1,8 @@
 public class HorizontalTransporter extends AbstractItem{
 
     private int capacity;
+    private int[] farmResults;
+    private int[] consumerResults;
 
     HorizontalTransporter (Grid grid, int x, int y, int cap) {
         this.grid = grid;
@@ -8,21 +10,43 @@ public class HorizontalTransporter extends AbstractItem{
         this.yCoordinate = y;
         this.capacity = cap;
         this.grid.registerItem(x, y, this);
+        farmResults = new int[this.grid.getWidth()];
+        consumerResults = new int[this.grid.getWidth()];
+        for(int i = 0; i < this.grid.getWidth(); i++) {
+            farmResults[i] = -1;
+        }
+        for(int i = 0; i < this.grid.getHeight(); i++) {
+            consumerResults[i] = -1;
+        }
 
     }
 
     @Override
     public void process(TimeStep timeStep) {
-        int foundFarm = this.farmCheck();
-        if(foundFarm >= 0) {
-            int foundDest = this.destinationCheck();
-            if(foundDest >= 0) {
-                if(positionCheck(foundFarm, foundDest)) {
-                    this.transportGoods(foundFarm, foundDest,
-                            this.stockCheck(foundFarm));
+        this.farmCheck();
+        for(int i: farmResults) {
+            if(i != -1) {
+                destinationCheck(i);
+                for (int j : consumerResults) {
+                    if(j != -1) {
+                        if(positionCheck(i, j)) {
+                            transportGoods(i, j, this.stockCheck(i));
+                        }
+                    }
+
                 }
             }
         }
+
+//        if() {
+//            int foundDest = this.destinationCheck();
+//            if(foundDest >= 0) {
+//                if(positionCheck(foundFarm, foundDest)) {
+//                    this.transportGoods(foundFarm, foundDest,
+//                            this.stockCheck(foundFarm));
+//                }
+//            }
+//        }
     }
 
     private void transportGoods(int start, int finish, int nutrition) {
@@ -39,33 +63,51 @@ public class HorizontalTransporter extends AbstractItem{
     private int stockCheck(int y) {
         int tempStock = this.grid.getStockAt(xCoordinate, y);
 
-        if((tempStock - this.capacity) < 0 && tempStock > 0) {
+        if (tempStock == 0) {
+            return 0;
+        } else if((tempStock - this.capacity) < 0 && tempStock > 0) {
             return tempStock;
         } else {
             return this.capacity;
         }
     }
 
-    private int farmCheck() {
+    private void farmCheck() {
         for(int i = 0; i < this.grid.getWidth(); i++) {
             if(this.grid.getItem(xCoordinate, i) instanceof CornFarmer ||
                     this.grid.getItem(xCoordinate, i) instanceof RadishFarmer) {
                 if(this.grid.getItem(xCoordinate, i).getStock() > 0) {
-                    return i;
+                    farmResults[i] = i;
                 }
             }
         }
-        return -1;
     }
 
-    private int destinationCheck() {
-        for(int i = 0; i < this.grid.getWidth(); i++) {
-            if(this.grid.getItem(xCoordinate, i) instanceof Rabbit ||
-                    this.grid.getItem(xCoordinate, i) instanceof Beaver) {
-                return i;
+    private void destinationCheck(int farmPos) {
+        if (farmPos > this.yCoordinate) {
+            for (int i = 0; i < this.yCoordinate; i++) {
+                if(this.grid.getItem(xCoordinate, i) instanceof Rabbit ||
+                        this.grid.getItem(xCoordinate, i) instanceof Beaver) {
+                    consumerResults[i] = i;
+                }
+            }
+        } else {
+            for(int i = this.grid.getWidth(); i > this.yCoordinate; i--) {
+                if(this.grid.getItem(xCoordinate, i) instanceof Rabbit ||
+                        this.grid.getItem(xCoordinate, i) instanceof Beaver) {
+                    consumerResults[i] = i;
+                }
             }
         }
-        return -1;
+
+
+//        for(int i = 0; i < this.grid.getWidth(); i++) {
+//            if(this.grid.getItem(xCoordinate, i) instanceof Rabbit ||
+//                    this.grid.getItem(xCoordinate, i) instanceof Beaver) {
+//                return i;
+//            }
+//        }
+//        return -1;
     }
 
     @Override
